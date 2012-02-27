dbf02f52fa50cce68949aeaf6d1e8d51a7c22335
Tim Visher <tim.visher@gmail.com>
2012-02-26 21:30:40 -0500

    Bumping Version Number. EOM

--- emacs/vimgolf.el
+++ emacs/vimgolf.el
@@ -5,7 +5,7 @@
 ;; Author: Tim Visher <tim.visher@gmail.com>
 ;; Maintainer: Tim Visher <tim.visher@gmail.com>
 ;; Created: 2011-11-02
-;; Version: 0.9.0
+;; Version: 0.9.1
 ;; Keywords: games vimgolf vim
 
 ;; This file is not part of GNU Emacs

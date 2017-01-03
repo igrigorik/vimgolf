d05410ec8c4fd7191f982f76640cb1974551395b
Harry Venables <venablesh@yahoo.co.uk>
2017-01-03 20:20:28 +0000

    Removed unnecessary Rspec configuration from support mongoid

--- spec/support/mongoid.rb
+++ spec/support/mongoid.rb
@@ -33,18 +33,3 @@ module Mongoid
     end
   end
 end
-
-RSpec.configure do |configuration|
-  configuration.include Mongoid::Matchers
-
-  # rspec-rails 3 will no longer automatically infer an example group's spec type
-  # from the file location. You can explicitly opt-in to the feature using this
-  # config option.
-  # To explicitly tag specs without using automatic inference, set the `:type`
-  # metadata manually:
-  #
-  #     describe ThingsController, :type => :controller do
-  #       # Equivalent to being in spec/controllers
-  #     end
-  configuration.infer_spec_type_from_file_location!
-end

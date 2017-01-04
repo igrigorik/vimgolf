ef070b043a4e4b853c12335bbf1a9a41b5d914d6
Nelson Senna <onelsonsenna@gmail.com>
2017-01-04 14:39:56 -0200

    Use new Hash syntax in Comment model and added tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment
   include Mongoid::Document
-  field :nickname, :type => String
-  field :comment, :type => String
+  field :nickname, type: String
+  field :comment, type: String
 
-  embedded_in :entry, :inverse_of => :comments
+  embedded_in :entry, inverse_of: :comments
 
   validates_presence_of :nickname
   validates_presence_of :comment
-  validates_length_of   :comment, :minimum => 1
+  validates_length_of   :comment, minimum: 1
 end

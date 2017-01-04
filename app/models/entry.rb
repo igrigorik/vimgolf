bc41d12013b8adbd699ca34ff686fd239e3da0a4
Nelson Senna <onelsonsenna@gmail.com>
2017-01-04 14:28:36 -0200

    Refactoring on Entry model
    Entry model has two new methods:
    
    * any_owned_by: Checks if a user has at least one entry in the given
      criteria (most of the time it's scoped by Challenge model) and return
      true or false.
    
    * top_by_user: Returns an array containing user's most recent entries
      with best score.
    
    Instance "owner?" method was renamed to "owned_by?" to read better.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -11,7 +11,18 @@ class Entry
 
   validates_length_of :script, minimum: 1, maximum: MAX_FILESIZE
 
-  def owner?(current_user)
+  # Returns true if the given user sent an entry
+  def self.any_owned_by?(current_user)
+    criteria.where(user_id: current_user.id).count > 0 if current_user.present?
+  end
+
+  # Returns best score entry per user
+  def self.top_by_user
+    criteria.sort_by { |e| [e.score, e.created_at] }.uniq(&:user_id)
+  end
+
+  # Returns true is an entry was created by given user
+  def owned_by?(current_user)
     user == current_user
   end
 end

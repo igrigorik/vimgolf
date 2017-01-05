0675a5d9cfa0e57a5feaca7d44ec6b81c9b3ec00
Nelson Senna <onelsonsenna@gmail.com>
2017-01-05 01:13:34 -0200

    Move test factories to a single file

---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do
+  factory :challenge do
+    title :test
+    description :test
+    input :a
+    output :b
+    diff :c
+    user
+  end
+
+  factory :entry do
+    script :a
+    created_at Time.now
+    user
+    score Faker::Number.between(1, 20)
+  end
+
+  factory :user, aliases: [:owner] do
+    name Faker::Name.name
+    nickname Faker::Internet.user_name
+    image Faker::Avatar.image
+    uid Faker::Number.number(5)
+    provider 'Github'
+  end
+end
+

a795070738a4c62810cceae564d38b56ebee27ca
Tyler Ewing <tewing10@gmail.com>
2017-01-14 17:15:34 -0500

    Add feature spec to test login via twitter

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+feature "Sign In" do
+  include OmniAuthHelper
+
+  before(:each) do
+    mock_omni_auth
+  end
+
+  scenario "as a first-time user" do
+    visit root_path
+
+    click_link "Sign in with Twitter"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text "Welcome the science guy"
+  end 
+
+  scenario "as an existing user" do
+    user = User.create!(
+      nickname: "foo",
+      name: "bar",
+      uid: "123545",
+      image: "foo",
+      provider: "twitter"
+    )
+
+    visit root_path
+
+    click_link "Sign in with Twitter"
+
+    expect(page).to have_text "Welcome the science guy"
+    expect(user.reload.name).to eq("bill nye")
+  end
+end

48c3794cf87b6081c536111a0699b37a3d8431b0
Harry Venables <harry@tagadab.com>
2017-01-19 17:02:57 +0000

    Started writing the feature spec for entries

---
+++ spec/features/entry_feature_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+feature "Entries for Challenges" do
+  include OmniAuthHelper
+
+  before(:each) do
+    mock_omni_auth
+  end
+
+  scenario 'can create an entry for a challenge' do
+
+  end
+
+  scenario 'can delete an entry' do
+
+  end
+
+  scenario 'can comment on an entry' do
+
+  end
+
+end

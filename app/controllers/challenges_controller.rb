6ecdc7ec43731aa20d8d312a1bca39aa5b55b618
Timothée Gauthier <itsumo.sibyllin@gmail.com>
2018-05-15 12:26:32 +0200

    Challenge page, in JSON, limit useless computation
    From 1.7 seconds to 0.005s in development
    
    How :
    - avoid downloading all 'entries' during the 'Challenge.find'
    - do not compute leaderboard
    
    How to get the metrics :
    
    First, the seed with lot of entries :
      bundle exec rake db:drop db:setup challenges=1 users=6000 entries=20000
    
    Before commit
      curl "http://localhost:8080/challenges/5ae92b89cfa4a4124f6507f2.json"
    
      I, [2018-05-02T05:22:39.569559 #2589]  INFO -- : Completed 200 OK in 1743ms (Views: 0.2ms)
      127.0.0.1 - - [02/May/2018:05:22:39 +0200] "GET /challenges/5ae92b89cfa4a4124f6507f2.json HTTP/1.1" 200 - 1.7885
    
    After commit
      curl "http://localhost:8080/challenges/5ae92b89cfa4a4124f6507f2.json"
    
      I, [2018-05-02T05:25:42.536583 #2591]  INFO -- : Completed 200 OK in 5ms (Views: 0.2ms)
      127.0.0.1 - - [02/May/2018:05:25:42 +0200] "GET /challenges/5ae92b89cfa4a4124f6507f2.json HTTP/1.1" 200 - 0.0241
    
    Refactor :
      - Add describe/context to challenges_controller_spec.rb
      - update factory :entry , change script from 'Symbol' to 'String' or
      the view will fail to render

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -49,28 +49,19 @@ class ChallengesController < ApplicationController
   end
 
   def show
-    @challenge = Challenge.find(params['id']) rescue nil
+    # Limit to id to avoid downloading uneccessary entries
+    @challenge = Challenge.only(:id).find(params['id']) rescue nil
     return redirect_to root_path if @challenge.nil?
 
-    # TODO, there is a better way to do this...
-    users = @challenge.entries.map {|e| e.user_id }.uniq
-    @users = User.where(:_id.in => users).inject({}) {|h,u| h[u.id] = u; h}
-
     respond_to do |format|
-      format.json {
-        render :json => {
-          'in' => {
-            'data' => @challenge.input,
-            'type' => @challenge.input_type
-          },
-          'out' => {
-            'data' => @challenge.output,
-            'type' => @challenge.output_type
-          },
-          'client' => Vimgolf::VERSION
-      }}
+      format.json { render :json => json_show(params['id']) }
 
-      format.html
+      format.html {
+        # TODO, there is a better way to do this...
+        @challenge = Challenge.find(params['id'])
+        users = @challenge.entries.map {|e| e.user_id }.uniq
+        @users = User.where(:_id.in => users).inject({}) {|h,u| h[u.id] = u; h}
+      }
     end
   end
 
@@ -79,4 +70,23 @@ class ChallengesController < ApplicationController
   def challenge_params
     params.require(:challenge).permit!
   end
+
+  def json_show(challenge_id)
+    challenge = Challenge
+      .only(:input, :input_type, :output, :output_type)
+      .find(challenge_id)
+
+    {
+      'in' => {
+        'data' => challenge.input,
+        'type' => challenge.input_type
+      },
+      'out' => {
+        'data' => challenge.output,
+        'type' => challenge.output_type
+      },
+      'client' => Vimgolf::VERSION
+    }
+  end
+
 end
--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -2,26 +2,72 @@ require 'spec_helper'
 
 describe ChallengesController do
 
-  it "should require login to create challenge" do
-    request.accept = 'application/json'
-    post "create", :format => :json
+  describe "#create" do
+    it "should require login to create challenge" do
+      post "create", :format => :json
 
-    expect(response.status).to eq(302)
+      expect(response.status).to eq(302)
+    end
   end
 
-  it "should allow download of challenge without login" do
-    create(
-      :challenge,
-      title: "foo",
-      description: "bar",
-      input: "baz",
-      output: "qux",
-      diff: "hoge"
-    )
-    request.accept = 'application/json'
-    get "show", params: { :id => Challenge.first.id.to_s, :format => :json }
+  describe "#show" do
+    context "json format" do
+      let(:challenge) {
+        create(
+          :challenge,
+          title: "foo",
+          description: "bar",
+          input: "baz",
+          input_type: "baz_type",
+          output: "qux",
+          output_type: "qux_type",
+          diff: "hoge"
+        )
+      }
+      it "should allow download of challenge without login" do
+        get "show", params: { id: challenge.id.to_s }, :format => :json
 
-    expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
+      end
+
+      it "Return object with defined attributes" do
+        get "show", params: { id: challenge.id.to_s }, :format => :json
+
+        json = ActiveSupport::JSON.decode(response.body)
+
+        expect(json).to eq({
+          "in"=>{"data"=>"baz", "type"=>"baz_type"},
+          "out"=>{"data"=>"qux", "type"=>"qux_type"},
+          "client"=> Vimgolf::VERSION
+        })
+      end
+    end
+
+    context "html format" do
+      render_views
+
+      let(:challenge) {
+        create(
+          :challenge,
+          title: "foo",
+          description: "bar",
+          input: "baz",
+          input_type: "baz_type",
+          output: "qux",
+          output_type: "qux_type",
+          diff: "hoge"
+        )
+      }
+
+      before do
+        challenge.entries <<  build(:entry)
+      end
+
+      it "works with html views" do
+        get "show", params: { :id => challenge.id.to_s }
+        expect(response.status).to eq(200)
+      end
+    end
   end
 
 end

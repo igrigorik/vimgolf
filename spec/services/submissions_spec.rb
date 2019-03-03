ae09f3e9b092aee16689c945bc3d5c29f35481e9
Timothée Gauthier <t.gauthier42@gmail.com>
2019-03-03 22:55:36 +0100

    Creator of a challenge can moderate all entries
    This closes #260

---
+++ spec/services/submissions_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Submissions do
+
+  describe '#user_id' do
+    let(:golfer) { create(:user) }
+    let(:owner) { create(:user) }
+    let(:challenge) { create(:challenge, user: owner) }
+
+    context 'user is the challenge owner' do
+      let(:submission) { Submissions.new(owner, challenge.id, 1) }
+      it 'return true' do
+        expect(submission.creator?).to be true
+      end
+    end
+
+    context 'user is not challenge owner' do
+      let(:submission) { Submissions.new(golfer, challenge.id, 1) }
+      it 'return true' do
+        expect(submission.creator?).to be false
+      end
+    end
+
+  end
+end

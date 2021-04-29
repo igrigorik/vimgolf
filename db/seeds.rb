0902e020ea1f73f00a2dcb928a7a84bdce2da7ed
Filipe Brandenburger <filbranden@gmail.com>
2021-04-29 11:34:17 -0700

    Update db/seeds.rb to use the new ActiveRecord models
    Tested that it works as expected:
    
      $ bundle exec rails db:setup challenges=11 users=73 entries=89
    
    And then browsing the website on localhost with the fake sample data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,47 +3,31 @@
 # or
 # bundle exec rake db:setup challenges=400 users=30 entries=100
 
-DatabaseHelper.create_collections
-
 params = {
   # create n challenges
   challenges: 0,
   # create n users, they create challenge or entries
   users: 0,
   # create n entries PER challenges
-  entries: 0,
-}.merge(ARGV[1..-1].map{|param| param.split('=')}.map{|k, v| [k.to_sym, v.to_i]}.to_h)
+  entries: 0
+}.merge(ARGV[1..].map { |param| param.split('=') }.map { |k, v| [k.to_sym, v.to_i] }.to_h)
 
 users = []
 
 params[:users].times do |i|
-  users.push(User.new(
+  users << User.create(
     name: Faker::Name.name,
     nickname: "#{Faker::Internet.user_name}_#{i}",
     image: Faker::Avatar.image,
     uid: Faker::Number.number(digits: 5),
     provider: 'Github',
     created_at: Time.now,
-    updated_at: Time.now,
-  ).as_document)
+    updated_at: Time.now
+  )
 end
-User.collection.insert_many(users)
-
-user_ids = User.pluck(:_id)
 
 params[:challenges].times do |i|
-  entries = []
-  params[:entries].times do |j|
-    entries.push(Entry.new(
-      script: "a\ra\na\r\nentries: #{j}\n\r\n",
-      created_at: Time.now,
-      updated_at: Time.now,
-      user_id: user_ids.sample,
-      score: Faker::Number.between(from: 2, to: 200)
-    ).as_document)
-  end
-
-  Challenge.collection.insert_one(Challenge.new(
+  challenge = Challenge.create(
     title: Faker::Book.title,
     description: "description #{i}",
     diff: 'diff',
@@ -51,9 +35,18 @@ params[:challenges].times do |i|
     input_type: 'input_type',
     output: 'output',
     output_type: 'output_type',
-    user_id: user_ids.sample,
+    user: users.sample,
     created_at: Time.now,
-    updated_at: Time.now,
-    entries: entries,
-  ).as_document)
+    updated_at: Time.now
+  )
+
+  params[:entries].times do |j|
+    challenge.entries.create(
+      script: "a\ra\na\r\nentries: #{j}\n\r\n",
+      created_at: Time.now,
+      updated_at: Time.now,
+      user: users.sample,
+      score: Faker::Number.between(from: 2, to: 200)
+    )
+  end
 end

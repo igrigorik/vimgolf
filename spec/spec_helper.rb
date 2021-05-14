96ef172b63df959832dea55f740752c2d0932bc5
Filipe Brandenburger <filbranden@gmail.com>
2021-05-14 11:00:50 -0700

    Replace poltergeist+phantomjs with Cuprite
    PhantomJS is no longer developed and Poltergeist is archived. The
    recommended replacement seems to be Culprite, which is a headless Chrome
    driver for Capybara.
    
    (I ran into this issue after upgrading to Fedora 34 and phantomjs
    stopped working due to the openssl version upgrade, so this was a good
    opportunity to shed the dependency on a deprecated project.)
    
    Tested:
    $ bundle exec rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,16 @@ SimpleCov.start 'rails'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
-require 'capybara/poltergeist'
+require 'capybara/cuprite'
 require 'shoulda/matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
-Capybara.javascript_driver = :poltergeist
+Capybara.javascript_driver = :cuprite
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800])
+end
 Capybara.server = :webrick
 
 Shoulda::Matchers.configure do |config|

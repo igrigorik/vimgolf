c9d24199b40e923612b6f9d5bc8e87dab46f8c54
Pedro <pietromaddera@gmail.com>
2023-05-06 18:00:53 +0200

    feat: updated User Profile page
    Now profile page shows the provider link based on the one users used for sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,15 @@ module ApplicationHelper
     link_to "@#{user}", profile_path(user)
   end
 
+  def provider_profile(user_object)
+    case user_object.provider.downcase
+    when 'github'
+      "<a href='https://www.github.com/#{user_object.nickname}' class='twitter-link' target='_new'>@#{user_object.nickname}</a>".html_safe
+    when 'twitter'
+      "<a href='https://www.twitter.com/#{user_object.nickname}' class='twitter-link' target='_new'>@#{user_object.nickname}</a>".html_safe
+    end
+  end
+
   def twitter_profile(user)
     "<a href='https://www.twitter.com/#{user}' class='twitter-link' target='_new'>@#{user}</a>".html_safe
   end
